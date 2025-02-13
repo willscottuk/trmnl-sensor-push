782034599fe68da901574e228e1511884f225866
Stu Eggerton <25424433+gitstua@users.noreply.github.com>
2025-02-13 16:39:21 +1100

    add friendly name

--- custom_components/trmnl_sensor_push/__init__.py
+++ custom_components/trmnl_sensor_push/__init__.py
@@ -29,6 +29,7 @@ def create_entity_payload(state) -> dict:
         "device_class": state.attributes.get('device_class', None),
         "unit_of_measurement": state.attributes.get('unit_of_measurement', None),
         "icon": state.attributes.get('icon', None),
+        "friendly_name": state.attributes.get('friendly_name', state.entity_id),
         "attributes": state.attributes
     }
     _LOGGER.debug("TRMNL: Created payload for %s: %s", state.entity_id, payload)
