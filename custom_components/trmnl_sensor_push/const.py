1bacec309cfa94a51fd65f4a251f1191b3fbe3b2
Stu Eggerton <gitstua@users.noreply.github.com>
2025-03-23 22:16:30 +1100

    Update const.py

--- custom_components/trmnl_sensor_push/const.py
+++ custom_components/trmnl_sensor_push/const.py
@@ -2,4 +2,4 @@
 DOMAIN = "trmnl_sensor_push"
 CONF_URL = "url"
 DEFAULT_URL = "https://usetrmnl.com/api/custom_plugins/XXXX-XXXX-XXXX-XXXX"  # Example URL
-MIN_TIME_BETWEEN_UPDATES = 1800  # 300 is 5 minutes in seconds, 1200 is every 30 mins
+MIN_TIME_BETWEEN_UPDATES = 1800  # 300 is 5 minutes in seconds, 1800 is every 30 mins
